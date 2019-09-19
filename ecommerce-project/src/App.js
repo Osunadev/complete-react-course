4b5026c866c41565ccc48bc5f98d362dbca92dc7
Omar Osuna <omosuna98@gmail.com>
2019-09-18 17:34:05 -0700

    Directory component added and style with scss

---
+++ ecommerce-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce-project/src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
