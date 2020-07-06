b0613a1a595d74ba63e6d9364767e2d26fd664c6
Omar Osuna <omar.alonso.osuna.angulo@uabc.edu.mx>
2020-07-06 14:11:12 -0700

    Added lodash.memoize to our selectCollection selector to memoize the whole function every time it gets called

--- ecommerce-project/src/redux/shop/shop.selectors.js
+++ ecommerce-project/src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const selectShop = (state) => state.shop;
 
@@ -10,10 +11,26 @@ export const selectCollectionsForPreview = createSelector([selectShopCollections
 	collections ? Object.keys(collections).map((collectionKey) => collections[collectionKey]) : []
 );
 
-export const selectCollection = (collectionUrlParam) =>
+/* selectCollection */
+// Since we're trying to memoizing a function that gets called everytime our
+// Collection Page gets updated (when redux updates the mapStateToProps)
+// we need to use Memoize from 'lodash' in order memoize the returned function
+// so that everytime it gets called, it doesn't 'create a new instance'
+
+// Memoize does the same idea of memoization as reselect does for our selectors,
+// except this time we're memoizing the return of our function which returns our selector
+
+// By wrapping this function is memoize, we're saying that whenever this function gets called
+// and receives collectionUrlParam, I want to memoize the return of this function (in this case
+// we return a selector). If this function gets called again with the same collectionUrlParam,
+// don't rerun this function because we'll return the same value as last time, which we've
+// memoized so just return the selector that's been stored.
+
+export const selectCollection = memoize((collectionUrlParam) =>
 	createSelector([selectShopCollections], (collections) =>
 		collections ? collections[collectionUrlParam] : null
-	);
+	)
+);
 
 export const selectIsCollectionFetching = createSelector([selectShop], (shop) => shop.isFetching);
 
